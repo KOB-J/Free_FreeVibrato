5b3872c8c95dbe3866cd075a7b705c7df5ddc263
kob-j <audiothingz@outlook.com>
2021-11-15 16:27:33 +0400

    add waveforms buttons

--- Source/WaveformsComponent.h
+++ Source/WaveformsComponent.h
@@ -19,6 +19,13 @@ public:
     {
         addAndMakeVisible(waveformsLabel);
         waveformsLabel.setText("Waveforms:", juce::dontSendNotification);
+
+        addAndMakeVisible(sineButton);
+        addAndMakeVisible(triangleButton);
+        addAndMakeVisible(squareButton);
+        addAndMakeVisible(sawtoothButton);
+        addAndMakeVisible(inverseSawtoothButton);
+
     }
 
     ~WaveformsComponent()
@@ -29,10 +36,22 @@ public:
     void resized() override
     {
         waveformsLabel.setBounds(getX(), getY(), getWidth(), getHeight() / 2);
+
+        int margin = getX();
+        sineButton.setBounds(margin, getHeight() / 3, getWidth() / 5, getHeight() / 3);
+        margin += getWidth() / 5;
+        triangleButton.setBounds(margin, getHeight() / 3, getWidth() / 5, getHeight() / 3);
+        margin += getWidth() / 5;
+        squareButton.setBounds(margin, getHeight() / 3, getWidth()  / 5, getHeight() / 3);
+        margin += getWidth() / 5;
+        sawtoothButton.setBounds(margin, getHeight() / 3, getWidth()  / 5, getHeight() / 3);
+        margin += getWidth() / 5;
+        inverseSawtoothButton.setBounds(margin, getHeight() / 3, getWidth() / 5, getHeight() / 3);
+
     }
 
 private:
 
     juce::Label waveformsLabel;
-    juce::ImageButton sineButton, triangleButton, squareButton, sawtoothButton, inverseSawtoothButton;
+    juce::TextButton sineButton, triangleButton, squareButton, sawtoothButton, inverseSawtoothButton;
 };
